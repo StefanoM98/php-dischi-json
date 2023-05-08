15e4208b02f0d557008d0c777a6d6a9549aae3fd
StefanoM98 <smartino030@gmail.com>
2023-05-08 14:20:55 +0200

    Add: disks on page

--- index.php
+++ index.php
@@ -12,12 +12,33 @@
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
     <!-- VUE JS -->
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <!-- LINK CSS -->
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
     <div id="app">
-
+        <header>
+            <img src="img/logo-small.svg" alt="">
+        </header>
+        <main>
+            <div class="container">
+                <div class="row justify-content-center row-cols-3">
+                    <div class="col" v-for="(disk, index) in disks" :key="index">
+                        <div class="card d-flex p-4 m-4 text-center align-items-center">
+                            <img class="card-img-top" :src="disk.poster" alt="Card image cap">
+                            <div class="card-body text-white">
+                                <h5 class="card-title">{{disk.title}}</h5>
+                                <p class="card-text">{{disk.author}}</p>
+                                <p class="card-text">{{ disk.year }}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
     </div>
+    <script src="js/script.js"></script>
 </body>
 
 </html>
